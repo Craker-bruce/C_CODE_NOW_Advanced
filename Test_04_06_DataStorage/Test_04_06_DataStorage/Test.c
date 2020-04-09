abca362235e448cd9914e3ae9365e9967caf7774
BranceCraker <1327968619@qq.com>
2020-04-09 23:02:52 +0800

    add 浮点数的存储

--- Test_04_06_DataStorage/Test_04_06_DataStorage/Test.c
+++ Test_04_06_DataStorage/Test_04_06_DataStorage/Test.c
@@ -12,6 +12,7 @@ int main()
 	return 0;
 }
 */
+/*
 void main()
 {
 	int count = 0;
@@ -21,4 +22,40 @@ void main()
 		count++;
 	}
 	printf("%d\n", count);
+}
+*/
+/*
+int main()
+{
+	int a = 9;
+	int* pa = &a;
+	printf("%d\n", a);
+	float* pf = (float*)&a;
+	printf("%f\n", *pf);
+	return 0;
+}
+
+*/
+/*
+void main()
+{
+	float f = 12.125;
+	//1.给出一个float数据能够手动推出内存的存储方式
+	//2.给出一个内存的float数据，要能够恢复出真实的float数据
+	//S符号位
+	//E阶码
+	//M尾码
+}
+*/
+
+int main()
+{
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	*pFloat = 9.0;
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	return 0;
 }
