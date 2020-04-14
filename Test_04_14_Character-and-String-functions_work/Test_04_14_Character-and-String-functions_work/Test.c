5ec8a97641c18f0dabdf4054598d005176608558
BranceCraker <1327968619@qq.com>
2020-04-14 21:43:10 +0800

    add work

--- Test_04_14_Character-and-String-functions_work/Test_04_14_Character-and-String-functions_work/Test.c
+++ Test_04_14_Character-and-String-functions_work/Test_04_14_Character-and-String-functions_work/Test.c
@@ -3,9 +3,7 @@
 #include<string.h>
 #include<assert.h>
 
-
 //3.Ä£ÄâÊµÏÖstrstr
-
 char* my_strstr(const char* str1, const char* str2)
 {
 	assert(str1 != NULL && str2 != NULL);
@@ -33,10 +31,10 @@ char* my_strstr(const char* str1, const char* str2)
 int main()
 {
 	char str1[] = "This is a simple string";
-	char* str2;
+	char* str2 = "simple";
 	printf("%s\n", str1);
-	str2 = my_strstr(str1, "simple");
-	printf("%s\n", str2);
+	char *str3 =  my_strstr(str1, str2);
+	printf("%s\n", str3);
 	return 0;
 }
 
