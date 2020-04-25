f62dd6f15d3ea1db63ac94bf892d5daf562e6694
BranceCraker <1327968619@qq.com>
2020-04-25 12:31:09 +0800

    add  work2

--- Test_04_23_work2/Test_04_23_work2/Work.c
+++ Test_04_23_work2/Test_04_23_work2/Work.c
@@ -1,8 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include<string.h>
+#include<assert.h>
+
+//写一个函数，判断一个字符串是否为另外一个字符串旋转之后的字符串。
+
+
+//字符串左旋
+//三种方式  1、如下   2、 strnapy  strncat  3、reverse
+void LeftRound(char* str, int n)
+{
+	assert(str != NULL);
+	int len = strlen(str);
+	int step = n % len;
+	while (step-- != 0)
+	{
+		char tmp = str[0];
+		for (int i = 0; i < len - 1; ++i)
+		{
+			str[i] = str[i + 1];
+		}
+		str[len - 1] = tmp;
+	}
+}
+
+void main()
+{
+	char str[] = "ABCDEFGHIJK";
+	int n = 2;
+	LeftRound(str, n);
+	printf("str = %s\n", str);
+}
 
 
 
+/*
 //编写一个函数找出这两个只出现一次的数字
 int FindNumber(int ar[], int n)
 {
@@ -18,7 +50,8 @@ void main()
 	int ar[] = { 1,2,4,3,7,7,3,2,1 };
 	int n = sizeof(ar) / sizeof(ar[0]);
 	printf("value = %d\n", FindNumber(ar, n));
-}/*
+}
+/*
 */
 /*
 //写一个宏，计算结构体中某变量相对于首地址的偏移，并给出说明
@@ -29,7 +62,7 @@ typedef struct Test
 	char b;
 	double c;
 }Test;
-#define offset() 
+#define offset()
 void main()
 {
 	printf("offset = %d\n", offset(Structure,Test.a));
